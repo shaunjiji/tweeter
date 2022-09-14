b7fbcf55da65517899204986e791839d09a1f78b
Shaun <shaunjiji123@gmail.com>
2022-09-14 21:18:28 +0000

    Added Newton image in index html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,9 +1,60 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// /*
+//  * Client-side JS logic goes here
+//  * jQuery is already loaded
+//  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+//  */
 
 
 
 
+// // Test / driver code (temporary). Eventually will get this from the server.
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//   "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//   "created_at": 1461116232227
+// }
+
+// const createTweetElement = function (tweetData) {
+// // declare variable called
+// // this function should extract text from content property (from tweetData) and store that as $tweet.text 
+// const $tweet = $(`
+
+//   <article>
+//       <div class="tweetheader">
+//         <div>
+//       <i class="fa-solid fa-face-sunglasses"></i>
+//       <span>${tweetData.user.name}</span>
+//        </div>
+//       <span>${tweetData.handle}</span>
+//       </div>
+      
+//       <p>${tweetData.content.text}</p>
+
+//       <footer class="tweetfooter">
+//       <span> ${tweetData.created_at}</span>
+//       <div>
+//       <i class="fa-solid fa-flag"></i>
+//       <i class="fa-solid fa-retweet"></i>
+//       <i class="fa-solid fa-heart"></i>
+//      </div>
+//      </footer>
+//       </article>
+
+
+// `);
+
+
+
+
+// return $tweet;
+// }
+
+
+
+// const $tweet = createTweetElement(tweetData);
